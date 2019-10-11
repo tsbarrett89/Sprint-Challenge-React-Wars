b86e9c0fa1a84b35f472f4a59a30781df28bc853
Thomas Barrett <tsbarrett89@gmail.com>
2019-10-11 10:41:12 -0600

    got character cards laid out and started styling

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const CharCard = styled.div `
+    Background: #DCC58B;
+    width: 30%;
+`
+
 const CharacterCard = props => {
     return (
-        <div></div>
+        <CharCard>
+            <h3>{props.name}</h3>
+            <p>Birth Year: {props.birthYear}</p>
+            <p>Mass: {props.mass}</p>
+            <p>Height: {props.height}</p>
+        </CharCard>
     )
 }
 
--- starwars/src/components/CharacterGrid.js
+++ starwars/src/components/CharacterGrid.js
@@ -4,7 +4,37 @@ import CharacterCard from './CharacterCard';
 import styled from 'styled-components';
 
 function CharacterGrid (){
+const [character, setCharacter] = useState([]);
 
+useEffect(() => {
+    axios
+        .get('https://swapi.co/api/people/')
+        .then (response => {
+            console.log(response);
+            setCharacter(response.data.results);
+        })
+}, [])
+
+const CharGrid = styled.div `
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+`
+
+return (
+    <CharGrid>
+        {Array.from(character).map((character, index) => {
+            return (
+                <CharacterCard
+                    key = {index}
+                    name = {character.name}
+                    birthYear = {character.birth_year}
+                    mass = {character.mass}
+                    height = {character.height} 
+                />)
+        })}
+    </CharGrid>
+)
 }
 
 export default CharacterGrid
